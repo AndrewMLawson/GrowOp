d7eecf05f304528d58621123d02633edae213953
AndrewMLawson <55327691+AndrewMLawson@users.noreply.github.com>
2021-01-17 10:04:32 -0800

    Update App.js to support sell functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import CoopViewComponent from './components/CoopViewComponent';
-import ProducerViewComponent from './components/ProducerViewComponent'
+import ProducerViewComponent from './components/ProducerViewComponent';
+import BuyerSheet from './components/BuyerSheet';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
  
@@ -20,11 +21,15 @@ function App() {
             <li>
               <Link to="/producer-view">Producer View</Link>
             </li>
+            <li>
+              <Link to="/producer-sell">Producer - Sell</Link>
+            </li>
           </ul>
         </nav>
         <Switch>
           <Route path="/coop-view" component={CoopViewComponent} />
           <Route path="/producer-view" component={ProducerViewComponent} />
+          <Route path="/producer-sell" component={BuyerSheet} />
         </Switch>
       </div>
     </BrowserRouter>
